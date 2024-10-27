d057d7ed78023b49751841c1f25048393ad033d8
woo0doo <duden5000@gmail.com>
2024-10-27 16:25:12 +0900

    feat: 상수 클래스 생성

---
+++ src/main/java/com/kcc/trioffice/global/constant/GlobalConstants.java
@@ -0,0 +1,11 @@
+package com.kcc.trioffice.global.constant;
+
+public class GlobalConstants {
+
+    public static final String CHAT_ROOM_ENTER = "/sub/chat/room/";
+    public static final String CHAT_ROOM_EMPLOYEES = "/sub/chatrooms/employees/";
+    public static final String DEFAULT_GROUP_IMAGE = "https://us.123rf.com/450wm/siamimages/siamimages1703/siamimages170303374/74232239-%EA%B7%B8%EB%A3%B9-%EC%82%AC%EB%9E%8C-%EC%95%84%EC%9D%B4%EC%BD%98.jpg";
+    public static final String DEFAULT_CREATE_CHAT_ROOM_MESSAGE = "대화방이 생성되었습니다.";
+    public static final String DEFAULT_DELETE_MESSAGE = "삭제된 메시지입니다.";
+    public static final String DEFAULT_DELETE_CHAT_ROOM_MESSAGE = "님이 채팅방을 나갔습니다.";
+}
