5f0c2149e424a1678f57bb639771301bd03ec43b
woo0doo <duden5000@gmail.com>
2024-10-29 11:35:01 +0900

    hotfix: redis 충돌 해결

--- src/main/java/com/kcc/trioffice/global/config/RedisConfig.java
+++ src/main/java/com/kcc/trioffice/global/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.kcc.trioffice.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -12,12 +13,20 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new JedisConnectionFactory();
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(host);
+        factory.setPort(port);
+        factory.setUsePool(true); // JedisPoolConfig 기본 설정을 따름 (30초에 한번씩 검사하여 60초 동안 비활성화 상태인 connection pool 에서 제거)
+        factory.setDatabase(1);
+        return factory;
     }
-
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
