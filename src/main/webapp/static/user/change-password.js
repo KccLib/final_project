9f3644453d4bce723b32e21af5fb65f9f4f5c250
Seong_gu <sunggu507@naver.com>
2024-11-04 17:01:21 +0900

    feat: 비밀번호 보기 화면 변경

--- src/main/webapp/static/user/change-password.js
+++ src/main/webapp/static/user/change-password.js
@@ -136,7 +136,17 @@ const thirdInput = document.getElementById("change-password-check-input");
 
 function togglePasswordVisibility(button, input) {
   button.addEventListener("click", () => {
-    input.type = input.type === "password" ? "text" : "password";
+    const icon = button.querySelector("i");
+
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    }
   });
 }
 
